6abe6ad8f47717a83948b4d231effea14e2eee45
Thiago <thiagodelgado.dev@gmail.com>
2023-05-14 11:55:26 -0300

    add proptypes

--- src/components/Metric.jsx
+++ src/components/Metric.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types'
 
-export const Metric = ({title,value,color,icon}) => {
+export const Metric = ({color,title,value,icon}) => {
   return (
 <div className="col-md-4 mb-4">
-  <div className="card border-left-primary shadow h-100 py-2">
+  <div className={`card border-left-${color} shadow h-100 py-2`}>
     <div className="card-body">
       <div className="row no-gutters align-items-center">
         <div className="col mr-2">
